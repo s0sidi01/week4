bd1c0c5b353a579a6172dc2a66d08d557340970a
s0sidi01 <s0sidi01@louisville.edu>
2025-11-08 12:11:01 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int[] age = { 20, 30, 60 };
+
+for (int i = 0; i < age.Length; i++)
+{
+    if (age[i] == 20)
+    {
+        Console.WriteLine("you're broke");
+    }
+    else if (age[i] == 30)
+    {
+        Console.WriteLine("you're somehow a little less broke");
+    }
+    else if (age[i] == 60)
+    {
+        Console.WriteLine("you're probably chilling");
+    }
+    else
+    {
+        Console.WriteLine("age is not on the list");
+    }
+}
